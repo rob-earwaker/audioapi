1992ba9554c1109b43612254dd6e81457d659139
Rob Earwaker <rob.earwaker@gmail.com>
2017-03-28 21:11:44 +0100

    Store uploaded audiofiles

--- api.py
+++ api.py
@@ -1,12 +1,36 @@
+import base64
+import uuid
+
 import flask
 
 
 app = flask.Flask(__name__)
 
 
-@app.route('/audiofile/upload', methods=['POST'])
+store = {}
+
+
+def new_id():
+    return base64.urlsafe_b64encode(uuid.uuid4().bytes)[:-2].decode('ascii')
+
+
+class AudioFile:
+    def __init__(self, id, data):
+        self.id = id
+        self.data = data
+
+
+@app.route('/audiofiles/upload', methods=['POST'])
 def upload_audiofile():
-    return flask.request.data
+    audiofile = AudioFile(new_id(), flask.request.data)
+    store[audiofile.id] = audiofile
+    return audiofile.id
+
+
+@app.route('/audiofiles/<id>', methods=['GET'])
+def retrieve_audiofile(id):
+    audiofile = store[id]
+    return audiofile.data
 
 
 if __name__ == '__main__':
--- test_api.py
+++ test_api.py
@@ -8,10 +8,6 @@ class TestApi(unittest.TestCase):
         api.app.config['TESTING'] = True
         self.app = api.app.test_client()
 
-    def test_root(self):
-        response = self.app.post('/audiofile/upload', data='Hello, World!')
-        self.assertEqual(b'Hello, World!', response.data)
-
 
 if __name__ == '__main__':
     unittest.main()
