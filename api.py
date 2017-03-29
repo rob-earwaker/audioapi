a8e0d66109e03ba1a5b530ffdb25488b7ec5fba8
Rob Earwaker <rob.earwaker@gmail.com>
2017-03-29 20:45:49 +0100

    Added length property to document

--- api.py
+++ api.py
@@ -19,20 +19,33 @@ class AudioFile:
         self.id = id
         self.data = data
 
+    def length(self):
+        return len(self.data)
+
 
 @app.route('/audiofiles/upload', methods=['POST'])
 def upload_audiofile():
     audiofile = AudioFile(new_id(), flask.request.data)
     store[audiofile.id] = audiofile
     return flask.jsonify({
-        '_links': {'self': {'href': '/audiofiles/{0}'.format(audiofile.id)}}
+        '_links': {
+            'self': {'href': '/audiofiles/{0}'.format(audiofile.id)},
+            'data': {'href': '/audiofiles/{0}/data'.format(audiofile.id)}
+        },
+        'length': audiofile.length()
     })
 
 
 @app.route('/audiofiles/<id>', methods=['GET'])
 def retrieve_audiofile(id):
     audiofile = store[id]
-    return audiofile.data
+    return flask.jsonify({
+        '_links': {
+            'self': {'href': '/audiofiles/{0}'.format(audiofile.id)},
+            'data': {'href': '/audiofiles/{0}/data'.format(audiofile.id)}
+        },
+        'length': audiofile.length()
+    })
 
 
 if __name__ == '__main__':
--- test_api.py
+++ test_api.py
@@ -9,11 +9,21 @@ class TestApi(unittest.TestCase):
         api.app.config['TESTING'] = True
         self.app = api.app.test_client()
 
-    def test_retrieve_audiofile_after_upload(self):
+    def test_upload_audiofile(self):
+        response = self.app.post('/audiofiles/upload', data=b'mockdata')
+        response = json.loads(response.data.decode('utf-8'))
+        self.assertIn('self', response['_links'])
+        self.assertIn('data', response['_links'])
+        self.assertEqual(8, response['length'])
+
+    def test_retrieve_audiofile(self):
         response = self.app.post('/audiofiles/upload', data=b'mockdata')
         response = json.loads(response.data.decode('utf-8'))
         response = self.app.get(response['_links']['self']['href'])
-        self.assertEqual(b'mockdata', response.data)
+        response = json.loads(response.data.decode('utf-8'))
+        self.assertIn('self', response['_links'])
+        self.assertIn('data', response['_links'])
+        self.assertEqual(8, response['length'])
 
 
 if __name__ == '__main__':
